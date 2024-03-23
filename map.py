5571482f60300916b94d8bfca4ae5896e658493a
beandrea <arkflame99@gmail.com>
2024-03-23 11:52:08 -0400

    set up

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import tile
 class Map:
-    def __init__(self) -> None:
+    def __init__(self, lock =  False, req = None) -> None:
         self.tiles = []
         self.neighbors = []
+        self.locked = lock
+        self.required = req
